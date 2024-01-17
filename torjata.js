48bd53358d37b7f2d4ff3d3f01fd065ce6e6212e
torjata <156916765+torjata@users.noreply.github.com>
2024-01-17 20:22:48 +0700

    Update torjata.js

--- torjata.js
+++ torjata.js
@@ -5,8 +5,9 @@ var filenameWithExtension = pathArray.pop();
 var filename = filenameWithExtension.split('.')[0];
 
 console.log(filename)
-if (filename !== 'index' || filename !== 'activities') {
-        const existingLink = document.getElementById('core-stylesheet');
-    if (existingLink) {
-        existingLink.remove();
+if (filename === 'index' || filename === 'activities') {
+        const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'style/admin-template/admin-template/assets/vendor/fonts/boxicons.css';
+    linkElement.id = 'core-stylesheet';
 }

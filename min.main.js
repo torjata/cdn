d362cc4e3d59f2308ffe0347f9dc10adc523f153
torjata <156916765+torjata@users.noreply.github.com>
2024-01-17 21:35:42 +0700

    Create min.main.js

---
+++ min.main.js
@@ -0,0 +1,1 @@
+var a=window.location.href,b=a.split("/"),c=b.pop(),d=c.split(".")[0];"index"!==d&&"activities"!==d&&Swal.fire({title:"อาจเกิดข้อผิดพลาด",text:"การใช้งานโคดร่วมกับไฟล์อื่นหรือใช้ผิดไฟล์ อาจเกิดข้อผิดพลาดขึ้นได้ โปรดติดต่อผู้พัฒนา หากท่านต้องการแก้ไขไฟล์",icon:"warning"});

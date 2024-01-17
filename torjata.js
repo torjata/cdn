46010180b93ad077517e96ea39b7c41504914036
torjata <156916765+torjata@users.noreply.github.com>
2024-01-17 20:12:18 +0700

    Update torjata.js

--- torjata.js
+++ torjata.js
@@ -5,7 +5,7 @@ var filenameWithExtension = pathArray.pop();
 var filename = filenameWithExtension.split('.')[0];
 
 console.log(filename)
-if (filename == 'index' || filename == 'activities' || 1==1) {
+if (filename == 'index2' || filename == 'activities2') {
     const linkElement = document.createElement('link');
     linkElement.rel = 'stylesheet';
     linkElement.href = 'https://katak.me/cdn/core.css';

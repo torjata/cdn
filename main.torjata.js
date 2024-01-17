a48032d10903b796ce8029282a9274c09c82aa77
torjata <156916765+torjata@users.noreply.github.com>
2024-01-17 20:26:57 +0700

    Update main.torjata.js

--- main.torjata.js
+++ main.torjata.js
@@ -1,1 +1,12 @@
+var currentURL = window.location.href;
+var pathArray = currentURL.split('/');
+var filenameWithExtension = pathArray.pop();
+var filename = filenameWithExtension.split('.')[0];
 
+console.log(filename)
+if (filename === 'index' || filename === 'activities') {
+        const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'style/admin-template/admin-template/assets/vendor/fonts/boxicons.css';
+    linkElement.id = 'core-stylesheet';
+}

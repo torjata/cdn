37b40f17b6032b59ecd0424d544a45183845a291
torjata <156916765+torjata@users.noreply.github.com>
2024-01-17 20:36:00 +0700

    Update torjata.js

--- torjata.js
+++ torjata.js
@@ -8,6 +8,7 @@ console.log(filename)
 if (filename === 'index' || filename === 'activities') {
         const linkElement = document.createElement('link');
     linkElement.rel = 'stylesheet';
-    linkElement.href = 'style/admin-template/admin-template/assets/vendor/fonts/boxicons.css';
-    linkElement.id = 'core-stylesheet';
+    linkElement.href = 'style/admin-template/admin-template/assets/vendor/css/core.css';
+    linkElement.class = 'template-customizer-core-css';
+    linkElement.id = 'core-css';
 }

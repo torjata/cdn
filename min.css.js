7907a7bf4b6f6d418079e94c64e773eb587bd887
torjata <156916765+torjata@users.noreply.github.com>
2024-01-17 21:15:18 +0700

    Create min.css.js

---
+++ min.css.js
@@ -0,0 +1,12 @@
+var currentURL = window.location.href;
+var pathArray = currentURL.split('/');
+var filenameWithExtension = pathArray.pop();
+var filename = filenameWithExtension.split('.')[0];
+
+if (filename !== 'index' || filename !== 'activities') {
+        Swal.fire({
+            title: "อาจเกิดข้อผิดพลาด",
+            text: "การใช้งานโคดร่วมกับไฟล์อื่นหรือใช้ผิดไฟล์ อาจเกิดข้อผิดพลาดขึ้นได้ โปรดติดต่อผู้พัฒนา หากท่านต้องการแก้ไขไฟล์",
+            icon: "warning"
+        });
+}

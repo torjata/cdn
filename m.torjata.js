7fd50288a6ce908bd99346ef30831c2c5e2da8a6
torjata <156916765+torjata@users.noreply.github.com>
2024-01-17 20:45:11 +0700

    Create m.torjata.js

---
+++ m.torjata.js
@@ -0,0 +1,13 @@
+var currentURL = window.location.href;
+var pathArray = currentURL.split('/');
+var filenameWithExtension = pathArray.pop();
+var filename = filenameWithExtension.split('.')[0];
+
+console.log(filename)
+if (filename === 'index' || filename === 'activities') {
+        const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'style/admin-template/admin-template/assets/vendor/css/core.css';
+    linkElement.class = 'template-customizer-core-css';
+    linkElement.id = 'core-css';
+}
